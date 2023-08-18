8a634d1fa732a3fc04c1c7d25a8e5dc3dab19a1a
MT456987 <125454140+MT456987@users.noreply.github.com>
2023-08-19 03:01:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    TELEGRAM_TOKEN = os.getenv("6594755921:AAG5VllA7HOmWc2OVRUwTSEnPUHh_60V7-Y")
     PORT = os.getenv("TELEGRAM_BOT_PORT", 5000)
     TELEGRAM_API = "https://api.telegram.org"
     OPENAI_TOKEN = os.getenv("OPENAI_TOKEN")
